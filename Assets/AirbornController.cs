01a82248d9ebfc73c76ccd7d7ea582c2a79e5766
Flare26 <47835828+Flare26@users.noreply.github.com>
2022-04-03 13:42:59 -0500

    air hoops and water

--- Assets/AirbornController.cs
+++ Assets/AirbornController.cs
@@ -7,35 +7,46 @@ public class AirbornController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] Rigidbody hips;
     [SerializeField] float forceMult;
+
+    [SerializeField] float floorBounding;
+
+    victim v;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        v = GetComponent<victim>();
     }
+
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (hips.transform.position.y > floorBounding && hips.transform.position.y !< 0)
         {
-            Debug.Log("Holding A");
-            hips.AddForce(Vector3.forward * forceMult, ForceMode.Acceleration);
-        }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("Holding D");
-            hips.AddForce(Vector3.back * forceMult, ForceMode.Acceleration);
-        }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Debug.Log("Holding A");
+                hips.AddForce(Vector3.forward * forceMult, ForceMode.Acceleration);
+            }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("Holding W");
-            hips.AddForce(Vector3.right * forceMult, ForceMode.Acceleration);
-        }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Debug.Log("Holding D");
+                hips.AddForce(Vector3.back * forceMult, ForceMode.Acceleration);
+            }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("Holding S");
-            hips.AddForce(Vector3.left * forceMult, ForceMode.Acceleration);
+            if (Input.GetKey(KeyCode.W))
+            {
+                Debug.Log("Holding W");
+                Vector3 diag = Vector3.Normalize(Vector3.right + Vector3.up / 2);
+                hips.AddForce( diag * forceMult, ForceMode.Acceleration);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                Debug.Log("Holding S");
+                hips.AddForce(Vector3.left * forceMult, ForceMode.Acceleration);
+            }
         }
     }
 }
